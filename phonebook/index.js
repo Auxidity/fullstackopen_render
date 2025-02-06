1faabc68c1094f1b85d327e4b8b400e256840f74
Daniel <daniel.kortesmaa@edu.turkuamk.fi>
2025-02-06 10:17:14 +0200

    uibuild

--- phonebook/index.js
+++ phonebook/index.js
@@ -11,6 +11,7 @@ morgan.token('body' , (request) => {
 });
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json())
