e6c77e84fdbe4cf2663ee1b6a140b519e21c46c1
Daniel <daniel.kortesmaa@edu.turkuamk.fi>
2025-02-06 09:50:52 +0200

    change

--- phonebook/index.js
+++ phonebook/index.js
@@ -120,7 +120,7 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${length} people</p><p>${date}</p>`);
 })
 
-const PORT = 6969
+const PORT = process.env.PORT || 6969
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
